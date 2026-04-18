fda06cd18bf4c21d18c3e180d3818d20bf79d4cf
Antoine <169061921+Antoine190@users.noreply.github.com>
2026-04-18 15:36:19 +0200

    Delete : lidar save

--- robot1/rasp/lidar_save/lidar_runtime.py
+++
@@ -1,9 +0,0 @@
-﻿"""Backward-compatible shim.
-
-Deprecated: import lidar_logic instead of lidar_runtime.
-"""
-
-try:
-    from .lidar_logic import *  # noqa: F401,F403
-except ImportError:
-    from lidar_logic import *  # noqa: F401,F403
